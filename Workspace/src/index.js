682d739cf05d6b57e179a0285baa1b2755fbee7f
Andre <phandoananh1609@gmail.com>
2022-11-11 18:38:05 +0700

    Template Engine + Layouts

--- Workspace/src/index.js
+++ Workspace/src/index.js
@@ -10,15 +10,19 @@ const port = 3000
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'))
 
  
 app.get('/', (req, res) =>{
   res.render('home');
 })
-
+app.get('/news', (req, res) =>{
+  res.render('news');
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
