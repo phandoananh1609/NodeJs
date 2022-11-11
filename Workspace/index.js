8424fdd38258491e4ff3c419aa3c6936d7105475
Andre <phandoananh1609@gmail.com>
2022-11-11 15:44:00 +0700

    Start

---
+++ Workspace/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+const Handlebars = require("handlebars");
+const template = Handlebars.compile("Name: {{name}}");
+console.log(template({ name: "Nils" }));
+
+app.use(morgan('combined'))
+
+var sum=(a,b)=>{
+    a=a*2
+    b=b*2
+    return a+b+3
+}
+var x=sum(1,3)
+ 
+app.get('/home', (req, res) =>
+  res.send('Con cac di me may')
+)
+app.get('/shop', (req, res) =>{
+  res.send('value is:' + x)
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
