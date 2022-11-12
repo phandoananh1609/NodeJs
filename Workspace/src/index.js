5f1faac299497b906f73f9be546e4c47ed1b88ef
Andre <phandoananh1609@gmail.com>
2022-11-12 08:03:11 +0700

    scss+static file

--- Workspace/src/index.js
+++ Workspace/src/index.js
@@ -6,6 +6,7 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public')));
 //HTTP logger
 app.use(morgan('combined'));
 
@@ -20,9 +21,7 @@ app.set('views', path.join(__dirname, 'resources\\views'))
 app.get('/', (req, res) =>{
   res.render('home');
 })
-app.get('/news', (req, res) =>{
-  res.render('news');
-})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
