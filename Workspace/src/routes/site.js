28fdb902f0ece5d0fd094e75e1698535c3bbda4e
Andre <phandoananh1609@gmail.com>
2022-11-14 22:28:49 +0700

    json viewer extension

--- Workspace/src/routes/site.js
+++ Workspace/src/routes/site.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const sctrl = require('../app/controllers/SitesController');
 
 // router.use('/:slug', sctrl.show)
-router.use('/news', sctrl.news);
-router.use('/search', sctrl.search);
-router.use('/:slug', sctrl.show);
-router.use('/', sctrl.home);
+router.get('/news', sctrl.news);
+router.get('/search', sctrl.search);
+router.get('/:slug', sctrl.show);
+router.get('/', sctrl.home);
 
 module.exports = router;
